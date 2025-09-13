5c2faa23737f2d9f51ab365d4f41f02904255c0c
Tiziana <rossitiziana90@gmail.com>
2025-09-12 22:09:46 -0300

    falta hacer validaciones

--- pryRossiEjercicio2/Boletos.Designer.cs
+++ pryRossiEjercicio2/Boletos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Boletos));
             lblDistancia = new Label();
             txtDistancia = new TextBox();
             lblDias = new Label();
@@ -40,65 +41,73 @@
             // lblDistancia
             // 
             lblDistancia.AutoSize = true;
-            lblDistancia.Location = new Point(51, 46);
+            lblDistancia.Location = new Point(58, 61);
             lblDistancia.Name = "lblDistancia";
-            lblDistancia.Size = new Size(55, 15);
+            lblDistancia.Size = new Size(70, 20);
             lblDistancia.TabIndex = 0;
             lblDistancia.Text = "Distancia";
             // 
             // txtDistancia
             // 
-            txtDistancia.Location = new Point(137, 46);
+            txtDistancia.Location = new Point(157, 61);
+            txtDistancia.Margin = new Padding(3, 4, 3, 4);
             txtDistancia.Name = "txtDistancia";
-            txtDistancia.Size = new Size(100, 23);
+            txtDistancia.Size = new Size(77, 27);
             txtDistancia.TabIndex = 1;
             // 
             // lblDias
             // 
             lblDias.AutoSize = true;
-            lblDias.Location = new Point(51, 96);
+            lblDias.Location = new Point(58, 128);
             lblDias.Name = "lblDias";
-            lblDias.Size = new Size(80, 15);
+            lblDias.Size = new Size(102, 20);
             lblDias.TabIndex = 2;
             lblDias.Text = "Cantidad Días";
             // 
             // nudDias
             // 
-            nudDias.Location = new Point(137, 94);
+            nudDias.Location = new Point(157, 126);
+            nudDias.Margin = new Padding(3, 4, 3, 4);
             nudDias.Maximum = new decimal(new int[] { 30, 0, 0, 0 });
             nudDias.Name = "nudDias";
-            nudDias.Size = new Size(100, 23);
+            nudDias.Size = new Size(77, 27);
             nudDias.TabIndex = 3;
             // 
             // lstResultado
             // 
             lstResultado.FormattingEnabled = true;
-            lstResultado.ItemHeight = 15;
-            lstResultado.Location = new Point(51, 209);
+            lstResultado.Location = new Point(53, 214);
+            lstResultado.Margin = new Padding(3, 4, 3, 4);
             lstResultado.Name = "lstResultado";
-            lstResultado.Size = new Size(186, 64);
+            lstResultado.Size = new Size(212, 84);
             lstResultado.TabIndex = 4;
             // 
             // btnCalcular
             // 
-            btnCalcular.Location = new Point(162, 332);
+            btnCalcular.BackColor = Color.FromArgb(255, 128, 128);
+            btnCalcular.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCalcular.ForeColor = SystemColors.ControlLightLight;
+            btnCalcular.Location = new Point(157, 331);
+            btnCalcular.Margin = new Padding(3, 4, 3, 4);
             btnCalcular.Name = "btnCalcular";
-            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.Size = new Size(108, 34);
             btnCalcular.TabIndex = 5;
             btnCalcular.Text = "Calcular";
-            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.UseVisualStyleBackColor = false;
             // 
             // Boletos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(352, 450);
+            ClientSize = new Size(325, 419);
             Controls.Add(btnCalcular);
             Controls.Add(lstResultado);
             Controls.Add(nudDias);
             Controls.Add(lblDias);
             Controls.Add(txtDistancia);
             Controls.Add(lblDistancia);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Boletos";
             Text = "BOLETOS FERRO";
             ((System.ComponentModel.ISupportInitialize)nudDias).EndInit();
